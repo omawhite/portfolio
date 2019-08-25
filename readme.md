7f2315518cf3061fb0f932cc0e456be859a7b544
Omar White <omawhite@indiana.edu>
2019-08-25 14:40:08 -0500

    more stuff

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 ## Louis White
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/fef10948-ee1a-4cb6-a0bb-d1a273b43c12/deploy-status)](https://app.netlify.com/sites/portfolio-louiswhite/deploys)
+
 This is my personal site....more coming soon
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default () => (
+  <Fragment>
+    Hi my name is <Styled.a href="http://example.com/">Omar</Styled.a>
+  </Fragment>
+)
