e6f7920b2181d75da045ee5a1c8f208851e6bee1
Omar White <omawhite@indiana.edu>
2019-08-24 22:17:06 -0500

    second commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <>
+      <h1>Welcome</h1>
+      <p>Hello, from Gatsby <span role="img" aria-label="hand emoji waving hello">👋</span></p>
+    </>
+  )
+}
