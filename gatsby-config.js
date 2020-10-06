e2bf0ce0e0bf46522d27a8ac8ff5b44e969e1f92
Omar White <omawhite@indiana.edu>
2020-10-06 04:04:07 +0000

    a bunch of stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,35 +7,27 @@ module.exports = {
     siteUrl: 'https://www.louiswhite.me',
     author: "Omar Louis White", 
     description: "Omar Louis White's Portfolio",
-    social: [
-      {
-        name: 'twitter',
-        url: 'https://twitter.com/louiswhite3019',
-      },
-      {
-        name: 'github',
-        url: 'https://github.com/omawhite',
-      },
-      {
-        name: 'dev.to',
-        url: 'https://dev.to/omawhite'
-      },
-      {
-        name: 'linkedin',
-        url: 'https://www.linkedin.com/in/omar-white-29b66ba3/'
-      },
-    ]
+    // social: [
+    //   {
+    //     name: 'twitter',
+    //     url: 'https://twitter.com/louiswhite3019',
+    //   },
+    //   {
+    //     name: 'github',
+    //     url: 'https://github.com/omawhite',
+    //   },
+    //   {
+    //     name: 'dev.to',
+    //     url: 'https://dev.to/omawhite'
+    //   },
+    //   {
+    //     name: 'linkedin',
+    //     url: 'https://www.linkedin.com/in/omar-white-29b66ba3/'
+    //   },
+    // ]
   },
   plugins: [
-    {
-      resolve: 'gatsby-theme-blog',
-      options: {
-        contentPath: 'static/posts', // the file path to your blog posts
-        basePath: '/', // the url for the root of your blog
-        assetPath: 'static/assets', // the file path to your assets folder
-        mdx: true, // whether or not to configure mdx for you
-      }
-    },
+    `gatsby-theme-material-ui`,
     `gatsby-plugin-netlify`,
     `gatsby-plugin-netlify-cache`,
     `gatsby-plugin-netlify-cms`, //https://www.gatsbyjs.org/packages/gatsby-plugin-netlify-cms/?=netlify
