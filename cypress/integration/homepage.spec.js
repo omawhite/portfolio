2cce3ee39b69cf0d4e71248c0cceb3c288585108
Omar White <omawhite@indiana.edu>
2021-05-01 13:57:33 -0500

    adds new test file

---
+++ cypress/integration/homepage.spec.js
@@ -0,0 +1,12 @@
+describe('Home page', () => {
+  before(() => {
+    cy.visit('/')
+  })
+
+  it('should land on homepage', () => {
+    cy.get('.css-ajj2pt-Header')
+    cy.get('article')
+    cy.get('article > header')
+    cy.get('.css-3rdz6s-Footer')
+  })
+})
