b8fc17c59b8094e731f7770d28060eaf52bd08e1
Omar White <omawhite@indiana.edu>
2021-10-02 14:54:09 -0500

    adds stories

---
+++ src/gatsby-theme-blog/components/bio-content.stories.tsx
@@ -0,0 +1,11 @@
+
+import BioContent from './bio-content'
+
+export default {
+  title: 'Components/bio-content',
+  component: BioContent,
+}
+
+const Template = (args) => <BioContent/>
+
+export const Default = Template.bind({})
